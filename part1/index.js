61a4ee8bf0acf6cd4f0d024c0b96e0afbe13be75
gauravsft <gauravsft@gmail.com>
2022-07-18 22:47:03 +0530

    video link

--- part1/index.js
+++ part1/index.js
@@ -8,18 +8,15 @@ const dns = require("dns");
 function getMeIp(req, res) {
     const body = req.body;
     const url = body.website_name;
-    let ip;
     dns.resolve4(url, (err, addresses) => {
         if (err) {
             console.err(err);
             return;
         }
-        ip=addresses[0];
-        console.log(addresses[0])
         res.status(200).send(addresses[0])
     });
     
     
 }
 
-app.listen(7000);
+app.listen(6000);
